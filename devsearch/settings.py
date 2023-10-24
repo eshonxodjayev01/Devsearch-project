9a35dfab0e84c43b27cdedce72494c482608911b
Sobitxon Xamidxanov <93433600+eshonxodjayev01@users.noreply.github.com>
2023-10-24 09:55:31 +0500

    Update settings.py

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -181,8 +181,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'eshonxodjayev001@gmail.com'
-EMAIL_HOST_PASSWORD = 'fvly cuft kmss dneu'
+EMAIL_HOST_USER = 'Your email address'
+EMAIL_HOST_PASSWORD = 'Your given password by google'
 
 
 # Static files (CSS, JavaScript, Images)
